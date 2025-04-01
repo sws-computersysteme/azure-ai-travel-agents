16db1d9498975f118e7db09f571e282832c3218e
Wassim Chegham <github@wassim.dev>
2025-04-02 00:29:25 +0200

    feat(accordion): add AccordionPreviewComponent with template and styles

---
+++ src/ui/src/app/components/accordion/accordion.component.ts
@@ -0,0 +1,48 @@
+import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideChevronDown } from '@ng-icons/lucide';
+import {
+  HlmAccordionContentComponent,
+  HlmAccordionDirective,
+  HlmAccordionIconDirective,
+  HlmAccordionItemDirective,
+  HlmAccordionTriggerDirective,
+} from '@spartan-ng/ui-accordion-helm';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
+
+@Component({
+	selector: 'accordion-preview',
+	standalone: true,
+	imports: [
+		HlmAccordionDirective,
+		HlmAccordionItemDirective,
+		HlmAccordionTriggerDirective,
+		HlmAccordionContentComponent,
+		HlmAccordionIconDirective,
+		HlmIconDirective,
+    NgIcon,
+	],
+	viewProviders: [provideIcons({ lucideChevronDown })],
+	template: `
+		<div hlmAccordion>
+			<div hlmAccordionItem class="max-h-[300px]" isOpened="true">
+				<button hlmAccordionTrigger>
+					<span>Agent Thought Process</span>
+					<ng-icon name="lucideChevronDown" hlm hlmAccIcon />
+				</button>
+				<hlm-accordion-content class="overflow-y-scroll">
+          <ng-content></ng-content>
+        </hlm-accordion-content>
+			</div>
+		</div>
+	`,
+  styles: [
+    `
+      hlm-accordion-content[data-state='open'] {
+        display: block;
+      }
+    `,
+  ]
+})
+export class AccordionPreviewComponent {}
