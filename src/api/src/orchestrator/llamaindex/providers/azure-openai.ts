f9854186e060d5d706753233e31c423ac84e3e83
Wassim Chegham <github@wassim.dev>
2025-04-07 10:21:25 +0200

    fix(azure-openai): remove error handling from OpenAI instance creation for streamlined execution

--- src/api/src/orchestrator/llamaindex/providers/azure-openai.ts
+++ src/api/src/orchestrator/llamaindex/providers/azure-openai.ts
@@ -2,15 +2,10 @@ import { openai } from "llamaindex";
 
 export const llm = async () => {
   console.log("Using Azure OpenAI");
-  try {
-    return openai({
-      azure: {
-        apiKey: process.env.AZURE_OPENAI_API_KEY,
-        deployment: process.env.AZURE_OPENAI_DEPLOYMENT_NAME,
-      },
-    })
-  } catch (error) {
-    console.error("Error creating OpenAI instance:");
-    console.error({error});
-  }
+  return openai({
+    azure: {
+      apiKey: process.env.AZURE_OPENAI_API_KEY,
+      deployment: process.env.AZURE_OPENAI_DEPLOYMENT_NAME,
+    },
+  });
 };
