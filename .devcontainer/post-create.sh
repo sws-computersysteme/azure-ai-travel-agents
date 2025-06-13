ce506ae5f1c7f488c9af6cf40c66c5d85e51344f
Wassim Chegham <github@wassim.dev>
2025-06-13 14:44:57 +0200

    fix: update devcontainer configuration and post-create script for improved setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,15 +1,30 @@
 sudo apt-get update && \
     sudo apt upgrade -y && \
     sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils libnss3 libnspr4 libdbus-1-3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libatspi2.0-0 libx11-6 libxcomposite1 libxdamage1 libxext6 libxfixes3 libxrandr2 libgbm1 libxcb1 libxkbcommon0 libpango-1.0-0 libcairo2 libasound2 build-essential && \
-    # Install Docker Model Plugin
-    sudo apt-get install docker-model-plugin && \ 
     sudo apt clean -y && \
     sudo rm -rf /var/lib/apt/lists/*
 
-echo Preparing .NET environment...
-dotnet tool update -g linux-dev-certs
-dotnet linux-dev-certs install
-sudo dotnet workload update
+# Install Docker Model Plugin
+echo Preparing Docker Model Plugin...
+
+# Run the following command to uninstall all conflicting packages:
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install the Docker packages.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-model-plugin
 
 # TODO: Uncomment the following when Foundy Local is available for Linux
 # See https://github.com/microsoft/Foundry-Local/discussions/82
