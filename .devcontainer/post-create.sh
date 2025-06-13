33371af6be772726ef4eb1292ba9ea31442a957f
Wassim Chegham <github@wassim.dev>
2025-06-13 14:09:54 +0000

    fix: update Docker-in-Docker feature version and streamline post-create script for improved setup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,30 +1,14 @@
-sudo apt-get update && \
-    sudo apt upgrade -y && \
-    sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils libnss3 libnspr4 libdbus-1-3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libatspi2.0-0 libx11-6 libxcomposite1 libxdamage1 libxext6 libxfixes3 libxrandr2 libgbm1 libxcb1 libxkbcommon0 libpango-1.0-0 libcairo2 libasound2 build-essential && \
-    sudo apt clean -y && \
-    sudo rm -rf /var/lib/apt/lists/*
-
-# Install Docker Model Plugin
-echo Preparing Docker Model Plugin...
+#!/bin/bash
 
 # Run the following command to uninstall all conflicting packages:
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+apt-get update -y
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# Use Docker script from script library to set things up - enable non-root docker, user vscode, using moby
+sudo /bin/bash ./docker-in-docker-debian.sh "true" "automatic" "true"
+sudo apt-get install -y docker-model-plugin
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-# Install the Docker packages.
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-model-plugin
+# Clean up
+apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
 
 # TODO: Uncomment the following when Foundy Local is available for Linux
 # See https://github.com/microsoft/Foundry-Local/discussions/82
