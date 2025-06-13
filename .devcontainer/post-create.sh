01c23f5e4589e9620236afd377db3b62bb78527d
Wassim Chegham <github@wassim.dev>
2025-06-13 12:33:50 +0200

    chore: add installation of Docker Model Plugin in post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,8 @@
 sudo apt-get update && \
     sudo apt upgrade -y && \
     sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils libnss3 libnspr4 libdbus-1-3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libdrm2 libatspi2.0-0 libx11-6 libxcomposite1 libxdamage1 libxext6 libxfixes3 libxrandr2 libgbm1 libxcb1 libxkbcommon0 libpango-1.0-0 libcairo2 libasound2 build-essential && \
+    # Install Docker Model Plugin
+    sudo apt-get install docker-model-plugin && \ 
     sudo apt clean -y && \
     sudo rm -rf /var/lib/apt/lists/*
 
